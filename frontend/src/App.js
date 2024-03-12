32c0ec08bbf7b0f0b50c91aae24d3e10d211dda1
diegog <diegogerlach312@gmail.com>
2024-03-12 15:16:53 +0100

    recovering files

---
+++ frontend/src/App.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ContactList from "./Userlist";
+import ContactCreate from "./CreateUser";
+import HomePage from "./HomePage";
+import PortfolioSearch from "./PortfolioSearch";
+import StockDataDisplay from "./StockLookup";
+import LoginPage from "./Login";
+import UpdatePortfolioForm from "./addUpdateStocks";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [totalPortfolioValue, setTotalPortfolioValue] = useState(0);
+
+  const handleLogin = async (usernameInput, password) => {
+    try {
+      const loginUrl = "http://127.0.0.1:5000/login"; // Change this when deploying "https://capstone-ml1.ew.r.appspot.com/login"
+      const response = await fetch(loginUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: usernameInput, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+          setIsLoggedIn(true);
+          setUsername(usernameInput);
+          localStorage.setItem("token", data.token); // Assuming the token is in data.token
+          localStorage.setItem("username", usernameInput);
+        } else {
+          alert("Login failed: " + data.message);
+        }
+      } else {
+        alert("Login failed with HTTP status: " + response.status);
+      }
+    } catch (error) {
+      alert("Login request failed: " + error.message);
+    }
+  };
+
+  return (
+    <Router>
+      <Routes>
+        {!isLoggedIn ? (
+          <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
+        ) : (
+          <Route
+            path="/"
+            element={
+              <HomePage
+                isLoggedIn={isLoggedIn}
+                username={username}
+                totalPortfolioValue={totalPortfolioValue}
+              />
+            }
+          />
+        )}
+        <Route path="/users" element={<ContactList />} />
+        <Route path="/create_user" element={<ContactCreate />} />
+        <Route
+          path="/portfolio"
+          element={
+            <PortfolioSearch
+              username={username}
+              setTotalPortfolioValue={setTotalPortfolioValue}
+            />
+          }
+        />
+        <Route path="/stock/:symbol" element={<StockDataDisplay />} />
+        <Route path = "/update_user" element={<UpdatePortfolioForm />} />
+        {/* ... other routes ... */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/addUpdateStocks.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Banner from "./Banner";
+
+const UpdatePortfolioForm = () => {
+  const [symbol, setSymbol] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+
+    try {
+      await axios.put(
+        "http://127.0.0.1:5000/update_user", // "https://capstone-ml1.ew.r.appspot.com/update_user" for deploying
+        { stocks: { [symbol]: parseInt(quantity, 10) } },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      alert("Portfolio updated successfully");
+      // Additional logic to refresh or update the UI
+    } catch (error) {
+      console.error("Error updating portfolio:", error);
+      alert("Error updating portfolio");
+    }
+  };
+
+  return (
+    <>
+      <Banner />
+      <form onSubmit={handleUpdate}>
+        <input
+          type="text"
+          value={symbol}
+          onChange={(e) => setSymbol(e.target.value)}
+          placeholder="Stock Symbol"
+          required
+        />
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          placeholder="Quantity"
+          required
+        />
+        <button type="submit">Update Portfolio</button>
+      </form>
+    </>
+  );
+};
+
+export default UpdatePortfolioForm;
